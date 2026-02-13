0d3c9cacd0398fe296ab79142ad11e8663978a2d
voidnamespace <voidnamespace@outlook.com>
2026-02-13 13:50:25 +0400

    added docker compose and fixed appsettings v2

---
+++ src/TransactionService.Infrastructure/Data/TransactionDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TransactionService.Infrastructure.Data;
+
+public class TransactionDbContextFactory
+    : IDesignTimeDbContextFactory<TransactionDbContext>
+{
+    public TransactionDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder =
+            new DbContextOptionsBuilder<TransactionDbContext>();
+
+        optionsBuilder.UseNpgsql(
+            "Host=localhost;Port=5432;Database=transactiondb;Username=postgres;Password=postgres");
+
+        return new TransactionDbContext(optionsBuilder.Options);
+    }
+}
