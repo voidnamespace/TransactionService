d4db837b17b4363f30d765aa53e533b7e4ddcded
voidnamespace <voidnamespace@outlook.com>
2026-02-13 13:35:53 +0400

    added middleware and extension for it

---
+++ src/TransactionService.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+namespace TransactionService.API.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            var response = new
+            {
+                error = ex.Message
+            };
+
+            await context.Response.WriteAsync(
+                JsonSerializer.Serialize(response));
+        }
+    }
+}
