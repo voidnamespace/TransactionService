dfa0ccfd8186925e7c7bc322444903fb0664a1c6
voidnamespace <voidnamespace@outlook.com>
2026-02-13 13:39:46 +0400

    init program cs added

---
+++ src/TransactionService.API/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using TransactionService.Application.Interfaces;
+using TransactionService.Infrastructure.Data;
+using TransactionService.Infrastructure.Messaging;
+using TransactionService.Infrastructure.Persistence.UnitOfWork;
+using TransactionService.Infrastructure.Repositories;
+using TransactionService.API.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TransactionDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEventBus, FakeEventBus>();
+
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(
+        typeof(TransactionService.Application.Commands.CreateTransfer.CreateTransferCommand).Assembly));
+
+
+var app = builder.Build();
+
+app.UseGlobalExceptionHandling();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
